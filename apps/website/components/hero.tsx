20a705d55785457f918040438400f3e38866902b
Simon Critchley <sicritchley@gmail.com>
2022-11-03 20:44:39 +0000

    updates to documentation site

--- apps/website/components/hero.tsx
+++ apps/website/components/hero.tsx
@@ -1,6 +1,6 @@
 import { DownloadIcon } from "@heroicons/react/solid";
 import Link from "next/link";
-import Image from "next/image";
+import Logo from "./Logo";
 
 export default function Hero() {
   return (
@@ -10,18 +10,18 @@ export default function Hero() {
           <div className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
             <div className="text-center">
               <p className="mb-12">
-                <span className="rounded bg-blue-50 px-2.5 py-1 text-xs font-semibold text-blue-600 tracking-wide uppercase">
+                <span className="rounded bg-blue-500 text-blue-50 px-2.5 py-1 text-xs font-semibold text-blue-600 tracking-wide uppercase">
                   Early Alpha Release
                 </span>
               </p>
-              <div>
-                <Image src="/logo.svg" height={100} width={300} />
+              <div className="flex justify-center">
+                <Logo height={100} />
               </div>
               <h1 className="text-5xl tracking-tight font-extrabold md:!text-6xl">
-                Multiverse
+                Multiverse Wallet
               </h1>
-              <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:!text-lg md:mt-5 md:!text-2xl md:max-w-3xl">
-                An open-source wallet for XRPL that lives in the browser.
+              <p className="mt-3 max-w-md mx-auto text-base text-gray-500 dark:text-gray-300 sm:!text-lg md:mt-5 md:!text-2xl md:max-w-3xl">
+                An open-source wallet for the XRPL that lives in the browser.
               </p>
               <p>
                 <Link href="/docs/getting-started">
@@ -37,17 +37,32 @@ export default function Hero() {
           </div>
         </div>
 
-        <div className="relative">
-          <div className="absolute inset-0 flex flex-col" aria-hidden="true">
-            <div className="flex-1" />
-            <div className="flex-1 w-full" />
-          </div>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6">
-            {/* <img
-              className="relative rounded-lg shadow-lg mx-auto"
-              src="/component.png"
+        <div className="flex relative rounded-xl">
+          <div className="p-5">
+            <img
+              className="relative rounded-lg shadow-lg mx-auto border border-slate-50"
+              src="/screenshot.png"
               alt="Component"
-            /> */}
+            />
+          </div>
+          <div className="p-5 text-lg flex items-center">
+            <ul className="flex flex-col gap-4">
+              <li>
+                <div className="font-extrabold text-2xl">Non-Custodial</div>
+                <div>
+                  Multiverse Wallet stores everything on your device, only you
+                  have access to your accounts, data and keys.
+                </div>
+              </li>
+              <li>
+                <div className="font-extrabold text-2xl">
+                  Developer Friendly
+                </div>
+                <div>
+                  Designed with web app developers in mind, integration is easy. See our <Link href="/docs/developers/introduction"><a>docs</a></Link> for more information.
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -61,7 +76,13 @@ export default function Hero() {
               className="col-span-2 flex justify-center items-center font-extrabold italic text-4xl"
               style={{ fontFamily: "'Work Sans', sans-serif" }}
             >
-              <a target="__blank" className="text-slate-700" href="https://xrplgrants.org/">XRPL Grants</a>
+              <a
+                target="__blank"
+                className="text-slate-700"
+                href="https://xrplgrants.org/"
+              >
+                XRPL Grants
+              </a>
             </div>
           </div>
         </div>
