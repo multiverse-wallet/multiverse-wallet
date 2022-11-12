ba507a2b73075d419c9516536fa131b2b4c31a3d
Simon Critchley <sicritchley@gmail.com>
2022-11-12 13:11:20 +0000

    adds white background to policy pages on website

--- apps/website/components/PrivacyPolicy.tsx
+++ apps/website/components/PrivacyPolicy.tsx
@@ -2,5 +2,5 @@ import React from "react";
 
 export default function PrivacyPolicy({ content }) {
   const html = `<a href="https://www.iubenda.com/privacy-policy/65910312" class="iubenda-white no-brand iubenda-noiframe iubenda-embed iub-legal-only iubenda-noiframe iub-body-embed" title="Privacy Policy">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>`;
-  return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
+  return <div className="bg-white rounded-xl" dangerouslySetInnerHTML={{ __html: html }}></div>;
 }
--- apps/website/components/TermsAndConditions.tsx
+++ apps/website/components/TermsAndConditions.tsx
@@ -2,5 +2,5 @@ import React from "react";
 
 export default function TermsAndConditions({ content }) {
   const html = `<a href="https://www.iubenda.com/terms-and-conditions/65910312" class="iubenda-white no-brand iubenda-noiframe iubenda-embed iubenda-noiframe iub-body-embed" title="Terms and Conditions">Terms and Conditions</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script>`;
-  return <div dangerouslySetInnerHTML={{ __html: html }}></div>;
+  return <div className="bg-white rounded-xl" dangerouslySetInnerHTML={{ __html: html }}></div>;
 }
